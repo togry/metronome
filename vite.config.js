bf47bce2034e4e46984fdfea8fa1b6bb7143431f
Tom Grydeland <tgry@norceresearch.no>
2026-03-04 23:49:56 +0100

    fixing deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/metronome/',
   plugins: [react()],
   build: {
-    // Inline all assets so the output is a single self-contained HTML file
     assetsInlineLimit: 100_000_000,
     rollupOptions: {
       output: {
